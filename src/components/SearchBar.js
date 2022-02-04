58661e2193c3b7d7a74ea8e4da4e4e929a232459
robwhite <robert.white3120@gmail.com>
2022-02-04 13:13:42 -0600

    created search bar and on change functionality

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,20 +3,39 @@ import { View, TextInput, StyleSheet } from 'react-native';
 import { Feather } from '@expo/vector-icons'
 
 
-const SearchBar = () => {
+const SearchBar = ( {term, onTermChange, onTermSubmit }) => {
   return (
-       <View style={style.background}>
-            <Feather size={30} name="search" />
-            <TextInput placeholder="Search" />
+       <View style={style.backgroundStyle}>
+            <Feather style={style.iconStyle} name="search" />
+            <TextInput 
+               style={style.inputStyle} 
+               placeholder="Search"
+               value={term}
+               onChangeText={onTermChange}
+               onEndEditing={onTermSubmit} 
+            />
        </View>
   )
 };
 
 const style = StyleSheet.create({
-     background: {
+     backgroundStyle: {
           backgroundColor: '#F0EEEE',
           height: 50,
           borderRadius: 5,
+          marginHorizontal: 15,
+          marginTop: 10,
+          flexDirection: 'row'
+     },
+     inputStyle: {
+          // borderColor: 'black',
+          // borderWidth: 1,
+          flex: 1,
+          fontSize: 18
+     },
+     iconStyle: {
+          fontSize: 35,
+          alignSelf: 'center',
           marginHorizontal: 15
      }
 });
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
 
 const SearchScreen = () => {
+     const [term, setTerm] = useState('');
+
      return (
           <View>
-               <SearchBar />
+               <SearchBar 
+                    term={term} 
+                    onTermChange={newTerm => setTerm(newTerm)}
+                    onTermSubmit={() => {alert(`You submitted this thing: ${term}`); setTerm('')}}
+               />
+               <Text>This is what i typed: {term}</Text>
           </View>
      )
 }
